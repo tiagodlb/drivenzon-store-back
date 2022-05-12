a3e189f9bac237732af61c7c463b80ad851fec40
Tiago <tigolimab223@gmail.com>
2022-05-12 00:28:29 -0300

    sign-in route e correção de bug na sign-up page

--- index.js
+++ index.js
@@ -2,21 +2,54 @@ import express, { json } from "express";
 import joi from "joi";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
+import cors from "cors";
+import { v4 as uuid } from "uuid";
 
 import db from "./db.js";
 
 const app = express();
 dotenv.config();
 app.use(json());
+app.use(cors());
+
+//login
+
+app.post("/sign-in", async (req, res) => {
+  //validar
+  const signInSchema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().required(),
+  });
+  const { error } = signInSchema.validate(req.body, { abortEarly: false });
+  if (error) {
+    return res.status(422).send(error.details.map((detail) => detail.message));
+  }
+
+  try {
+    const user = await db
+      .collection("users")
+      .findOne({ email: req.body.email });
+    if (!user) return res.sendStatus(404);
+    if (user && bcrypt.compareSync(req.body.password, user.password)) {
+      const token = uuid();
+      await db.collection("sessions").insertOne({ token, userId: user._id });
+      res.send({ token, name: user.name });
+    }
+  } catch (error) {
+    console.log("Error creating new user.");
+    console.log(error);
+    return res.sendStatus(500);
+  }
+});
 
 //cadastro
 app.post("/sign-up", async (req, res) => {
   //validar
   const signUpSchema = joi.object({
     name: joi.string().required(),
-    email: joi.email().required(),
+    email: joi.string().email().required(),
     password: joi.string().required(),
-    confirmpassword: joi.ref("password"),
+    confirmPassword: joi.ref("password"),
   });
 
   const { error } = signUpSchema.validate(req.body, { abortEarly: false });
