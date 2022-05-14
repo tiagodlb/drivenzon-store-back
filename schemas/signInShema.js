79608386f3f1f35df1e2ef97b421da88f5819e49
Tiago <tigolimab223@gmail.com>
2022-05-14 16:14:08 -0300

    Added schemas modules

---
+++ schemas/signInShema.js
@@ -0,0 +1,4 @@
+export const signInSchema = joi.object({
+  email: joi.string().email().required(),
+  password: joi.string().required(),
+});
---
+++ schemas/signUpSchema.js
@@ -0,0 +1,6 @@
+export const signUpSchema = joi.object({
+  name: joi.string().required(),
+  email: joi.string().email().required(),
+  password: joi.string().required(),
+  confirmPassword: joi.ref("password"),
+});
